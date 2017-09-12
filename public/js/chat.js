7cbcf7923f9582bca780f83d3723e6dfdfd05b76
hamza mir <hamxa46.mir@gmail.com>
2017-09-12 13:19:50 +0500

    full chatapp

---
+++ public/js/chat.js
@@ -0,0 +1,107 @@
+var socket = io();
+
+function scrollToBottom(){
+var messages=$('#messages');
+var newMessage=messages.children('li:last-child');
+
+var clientHeight=messages.prop('clientHeight');
+var scrollTop=messages.prop('scrollTop');
+var scrollHeight=messages.prop('scrollHeight');
+var newMessageHeight=newMessage.innerHeight();
+var lastMessageHeigth=newMessage.prev().innerHeight();
+if(clientHeight+scrollTop +newMessageHeight + lastMessageHeigth>=scrollHeight){
+  messages.scrollTop(scrollHeight);
+}
+};
+
+
+socket.on('connect', function() {
+  var params=$.deparam(window.location.search);
+  socket.emit('join',params,function(err){
+    if(err){
+      alert(err);
+      window.location.href='/';
+    }else{
+      console.log('no error occurred');
+    }
+  });
+});
+
+
+socket.on('disconnect', function() {
+    console.log('Disconnected from server');
+});
+
+
+socket.on('updatedUserList',function (users){
+var ol=$('<ul></ul>');
+users.forEach(function(user){
+  ol.append($('<li></li>').text(user));
+
+});
+$('#users').html(ol);
+
+
+
+});
+
+socket.on('newMessage',function (message){
+var formattedTime=moment(message.createdAt).format('h:mm a');
+var template=$('#message-template').html();
+var html=Mustache.render(template,{
+  text:message.text,
+  from:message.from,
+  createdAt:formattedTime
+});
+$('#messages').append(html);
+scrollToBottom();
+
+});
+
+socket.on('newLocationMessage',function(message){
+  var formattedTime=moment(message.createdAt).format('h:mm a');
+  var template=$('#location-message-template').html();
+  var html=Mustache.render(template,{
+    from:message.from,
+    url:message.url,
+    createdAt:formattedTime
+  });
+  scrollToBottom();
+
+  $('#messages').append(html);
+  scrollToBottom();
+
+
+});
+
+
+
+$('#message-form').on('submit',function(e){
+  e.preventDefault();
+  var messageTextBox=$('[name=message]')
+  socket.emit('createMessage',{
+    text:messageTextBox.val()
+  },function(){
+    messageTextBox.val('');
+  });
+});
+
+var locationButton=$('#send-location');
+locationButton.on('click',function(){
+  if(!navigator.geolocation){
+    return alert('geolocation not supported by your browser');
+  }
+
+  locationButton.attr('disabled','disabled').text('sending location ...');
+  navigator.geolocation.getCurrentPosition(function (position){
+    locationButton.removeAttr('disabled').text('send location');
+    socket.emit('createLocationMessage',{
+      latitude:position.coords.latitude,
+      longitude:position.coords.longitude
+    });
+  },function(){
+    locationButton.removeAttr('disabled').text('send location');
+
+    alert('unable to fetch the location');
+  });
+});
